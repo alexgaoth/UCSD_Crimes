9a32b09d6428a8988637459e9d130aa85c4e044e
alexgaoth <alexisobrenovic@gmail.com>
2025-10-22 21:07:37 -0700

    fixing routers

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';  
 import { ReportsProvider } from './context/ReportsContext.jsx';
 import App from './App.jsx';
 
 createRoot(document.getElementById('app')).render(
   <StrictMode>
-    <HashRouter>
       <ReportsProvider>
         <App />
       </ReportsProvider>
-    </HashRouter>
   </StrictMode>,
 );
