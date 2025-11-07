abf9bd4b1ff90e6b9a48cb30ccb68c591ecb4d64
alexgaoth <alexisobrenovic@gmail.com>
2025-11-06 17:45:33 -0800

    changed workflow

---
+++ app/src/supabase.js
@@ -0,0 +1,4 @@
+export const SUPABASE_CONFIG = {
+  url: 'https://edhfeokqefdtmkaoitpu.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkaGZlb2txZWZkdG1rYW9pdHB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzODA0MDcsImV4cCI6MjA3Nzk1NjQwN30.BH3tAAM3tLzsY1fd1XglLWoZRGLyGJMFtlaiXOyW-I0'
+};
