b56b1d859dce8da42ecf8338453cd77076d50e26
alexgaoth <alexisobrenovic@gmail.com>
2025-11-10 17:58:49 -0800

    seo improvments

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { ReportsProvider } from './context/ReportsContext.jsx';
 import App from './App.jsx';
@@ -10,12 +10,12 @@ import ScrollToTop from './ScrollToTop';
 createRoot(document.getElementById('app')).render(
   <StrictMode>
     <HelmetProvider>
-      <BrowserRouter basename="/UCSD_Crimes">
+      <HashRouter>
         <ScrollToTop />
         <ReportsProvider>
           <App />
         </ReportsProvider>
-      </BrowserRouter>
+      </HashRouter>
     </HelmetProvider>
   </StrictMode>,
 );
